41ff2b502c9d538c32c77c02896fad618c764809
sjj118 <sjj118@sjj118.com>
2016-08-12 17:45:33 +0800

    test

---
+++ a.bat
@@ -0,0 +1,6 @@
+set input_value=%1
+if defined  input_value (
+echo %1
+) else (
+echo 0
+)
--- push.bat
+++ push.bat
@@ -1,4 +1,9 @@
 git add -A
 git status
+set description=%1
+if defined description (
+git commit -m "%1"
+) else (
 git commit -m "%date:~0,-2%%time%"
+)
 git push
